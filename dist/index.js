368da3f7cba964edb7a410001fcbfa07fc755182
Jonathan Soukaseume <sksmejn@gmail.com>
2021-09-20 10:52:54 -0700

    Add initial files and folder structures. Changed tsconfig.json to have output and root directory of ts files and output files.

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+// use 'tsc index' to compile to js
+// use 'tsc --watch index' to compile dyanmically
+// Set up config file: 'tsc --init'
+// Change to ES6 compiled code in tsconfig.json // Default is ES5
+// Type is inferred once set
+let id = 5;
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+// use 'tsc index' to compile to js
+// use 'tsc --watch index' to compile dyanmically
+
+// Set up config file: 'tsc --init'
+// Change to ES6 compiled code in tsconfig.json // Default is ES5
+
+// Type is inferred once set
+let id: number = 5;
+
+console.log(id);
