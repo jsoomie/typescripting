ddeea370ca439a2e1fdec02c43eae88f1a043d12
Jonathan Soukaseume <sksmejn@gmail.com>
2021-09-20 10:55:55 -0700

    Added the tsc to remove comments from tsc files when compiling to js.

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,3 @@
 "use strict";
-// use 'tsc index' to compile to js
-// use 'tsc --watch index' to compile dyanmically
-// Set up config file: 'tsc --init'
-// Change to ES6 compiled code in tsconfig.json // Default is ES5
-// Type is inferred once set
 let id = 5;
+console.log(id);
