a19d8226cb4f5eec0fc28a46a8dae96191a9c3c3
Jonathan Soukaseume <sksmejn@gmail.com>
2021-09-20 12:33:50 -0700

    Added Interfaces examples in objects and Classes. Added classes and subclasses.

--- dist/index.js
+++ dist/index.js
@@ -5,8 +5,8 @@ let isPublished = true;
 let x = "Hello";
 let ids = [1, 2, 3, 4, 5];
 let doggy = [1, "Dog", true];
-let employee;
-employee = [
+let employees;
+employees = [
     [1, "Jon"],
     [2, "Mae"],
     [3, "Joey"],
@@ -49,3 +49,47 @@ const log = (msg) => {
     console.log(msg);
 };
 log(`Hello ${1}`);
+const employee = {
+    id: 2,
+    name: "Lacie"
+};
+const add = (x, y, z) => x + y + z;
+const sub = (x, y, z) => x - y - z;
+const multiply = (x, y, z) => x * y * z;
+class Person {
+    constructor(id, name, age, free) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.free = free;
+        console.log("Constructor ran");
+    }
+    register() {
+        return `${this.name} is now registered!`;
+    }
+}
+const jill = new Person(1, "Jill", 24, true);
+const jack = new Person(2, "Jack", 22, false);
+console.log(jill, jack);
+console.log(jill.register());
+class OtherPerson {
+    constructor(id, name, age, free) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.free = free;
+    }
+    register() {
+        return `${this.name} is registered!`;
+    }
+}
+const hailey = new OtherPerson(1, "Hailey", 20, true);
+console.log(hailey);
+class Employee extends OtherPerson {
+    constructor(id, name, age, free, position) {
+        super(id, name, age, free);
+        this.position = position;
+    }
+}
+const james = new Employee(3, "James", 27, true, "Developer");
+console.log(james.register());
--- src/index.ts
+++ src/index.ts
@@ -5,29 +5,29 @@
 // Change to ES6 compiled code in tsconfig.json // Default is ES5
 
 // Type is inferred once set
-// Basic primivites /////////////////////////////////////////////////
+// BASIC PRIMIVITIVES /////////////////////////////////////////////////////////////////////////////////
 let companyID: number = 5;
 let company: string = "My Name is Jonathan";
 let isPublished: boolean = true;
 
-// Any Types /////////////////////////////////////////////////
+// ANY TYPES /////////////////////////////////////////////////////////////////////////////////////////
 let x: any = "Hello";
 
-// Array TYPE[ARRAY] // Must conform to type /////////////////////////////////////////////////
+// Array TYPE[ARRAY] // Must conform to type /////////////////////////////////////////////////////////
 let ids: number[] = [1, 2, 3, 4, 5];
 
-// Tuples // Types must be in the order stated  /////////////////////////////////////////////////
+// TUPLES // Types must be in the order stated  /////////////////////////////////////////////////////////
 let doggy: [number, string, boolean] = [1, "Dog", true];
 // Tuple Array
-let employee: [number, string][];
+let employees: [number, string][];
 // Must be in the order stated once again
-employee = [
+employees = [
   [1, "Jon"],
   [2, "Mae"],
   [3, "Joey"],
 ];
 
-// Union - Combining different types to one /////////////////////////////////////////////////
+// UNION - Combining different types to one /////////////////////////////////////////////////////////////
 let id: string | number;
 
 id = 22;
@@ -52,7 +52,7 @@ enum Direction2 {
 }
 console.log(Direction2);
 
-// Objects /////////////////////////////////////////////////
+// OBJECTS /////////////////////////////////////////////////////////////////////////////////////////////
 // Messy way 
 const user: {
   id: number;
@@ -74,7 +74,7 @@ const users: Users = {
   name: "Lacie"
 }
 
-// Type Assertion //////////////////////////////////////////////////////////////////////////////////////////////////
+// TYPE ASSERTION //////////////////////////////////////////////////////////////////////////////////////////////////
 // Explicitly tells the compiler that we want to treat an entity as a different type
 // Two ways to achieve this.
 // 1.
@@ -86,7 +86,7 @@ let cid2: any = 1;
 let customerID2 = cid2 as number;
 
 
-// Functions wtih return //////////////////////////////////////////////////////////////////////////////////////////////////
+// FUNCTIONS wtih return //////////////////////////////////////////////////////////////////////////////////////////
 // const FUNCTION_NAME = (PARAM 1: TYPE, PARAM 2: TYPE): RETURN TYPE 
 const addnumbers = (x: number, y: number): number => {
   return x + y;
@@ -100,3 +100,102 @@ const log = (msg: string | number): void => {
   console.log(msg)
 }
 log(`Hello ${1}`);
+
+// INTERFACES //////////////////////////////////////////////////////////////////////////////////////////////////////
+// Just like using types lines 67 - 75
+// However, interfaces CANNOT be set to a union
+interface EmployeeInterface {
+  readonly id: number, // Readonly: cannot be changed down the line
+  name: string,
+  age?: number, // ? Means that it is optional to the variable
+}
+
+const employee: EmployeeInterface = {
+  id: 2,
+  name: "Lacie"
+}
+
+// Interfaces with functions
+// add interface at function variable
+interface MathFunctions {
+  (x: number, y: number, z: number): number
+}
+
+const add: MathFunctions = (x: number, y: number, z: number): number => x + y + z;
+const sub: MathFunctions = (x: number, y: number, z: number): number => x - y - z;
+const multiply: MathFunctions = (x, y, z) => x * y * z;
+
+
+
+// CLASSES /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Data modifiers = Public / Private / Protected
+class Person {
+  protected id: number
+  name: string
+  private age: number
+  free: boolean
+  // Private: Only accessible within the class
+  // Protected: Only accessible from within the class and any extending classes
+
+  // Contstrutors runs as soon as object is instantiated
+  constructor(id: number, name: string, age: number, free: boolean) {
+    this.id = id;
+    this.name = name;
+    this.age = age;
+    this.free = free;
+    console.log("Constructor ran");
+  }
+
+  register() {
+    return `${this.name} is now registered!`;
+  }
+}
+
+const jill = new Person(1, "Jill", 24, true);
+const jack = new Person(2, "Jack", 22, false);
+
+console.log(jill, jack);
+console.log(jill.register())
+
+
+// Interfaces with classes
+interface PersonInterface {
+  id: number,
+  name: string,
+  age: number,
+  free: boolean,
+  register(): string
+}
+class OtherPerson implements PersonInterface {
+  constructor(id: number, name: string, age: number, free: boolean) {
+    this.id = id;
+    this.name = name;
+    this.age = age;
+    this.free = free;
+  }
+
+  id
+  name
+  age
+  free
+
+  register() {
+    return `${this.name} is registered!`;
+  }
+}
+
+const hailey = new OtherPerson(1, "Hailey", 20, true);
+console.log(hailey);
+
+// Extending Classes 
+// Employee is a subclass of OtherPerson
+class Employee extends OtherPerson {
+  constructor(id: number, name: string, age: number, free: boolean, position: string) {
+    super(id, name, age, free) // Used for extending from parent class
+    this.position = position; // must instantiate 
+  }
+  position: string
+}
+
+const james = new Employee(3, "James", 27, true, "Developer");
+console.log(james.register());
