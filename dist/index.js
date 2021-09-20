aba682e8805bfb514879a87e660ff60b01837092
Jonathan Soukaseume <sksmejn@gmail.com>
2021-09-20 11:27:24 -0700

    Added unions, Enums, and objects types. Creating types beforehand and setting the variable to those types. Also formatted document.

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,5 @@
 "use strict";
-let id = 5;
+let companyID = 5;
 let company = "My Name is Jonathan";
 let isPublished = true;
 let x = "Hello";
@@ -9,5 +9,31 @@ let employee;
 employee = [
     [1, "Jon"],
     [2, "Mae"],
-    [3, "Joey"]
+    [3, "Joey"],
 ];
+let id;
+id = 22;
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 0] = "Up";
+    Direction1[Direction1["Down"] = 1] = "Down";
+    Direction1[Direction1["Left"] = 2] = "Left";
+    Direction1[Direction1["Right"] = 3] = "Right";
+})(Direction1 || (Direction1 = {}));
+console.log(Direction1);
+var Direction2;
+(function (Direction2) {
+    Direction2[Direction2["Up"] = 1] = "Up";
+    Direction2[Direction2["Down"] = 2] = "Down";
+    Direction2[Direction2["Left"] = 3] = "Left";
+    Direction2["Right"] = "down";
+})(Direction2 || (Direction2 = {}));
+console.log(Direction2);
+const user = {
+    id: 1,
+    name: "Alice",
+};
+const users = {
+    id: 1,
+    name: "Lacie"
+};
--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,12 @@
 
 // Type is inferred once set
 // Basic primivites
-let id: number = 5;
+let companyID: number = 5;
 let company: string = "My Name is Jonathan";
 let isPublished: boolean = true;
 
 // Any Types
-let x: any = "Hello"
+let x: any = "Hello";
 
 // Array TYPE[ARRAY] // Must conform to type
 let ids: number[] = [1, 2, 3, 4, 5];
@@ -19,10 +19,57 @@ let ids: number[] = [1, 2, 3, 4, 5];
 // Tuples // Types must be in the order stated
 let doggy: [number, string, boolean] = [1, "Dog", true];
 // Tuple Array
-let employee: [number, string] [];
+let employee: [number, string][];
 // Must be in the order stated once again
 employee = [
   [1, "Jon"],
   [2, "Mae"],
-  [3, "Joey"]
-]
+  [3, "Joey"],
+];
+
+// Union
+let id: string | number;
+
+id = 22;
+
+// Enum
+// By default, each value starts at 0 and enumerates from there
+enum Direction1 {
+  Up,
+  Down,
+  Left,
+  Right,
+}
+console.log(Direction1);
+
+// When given an explicit number, will enumerate from the given number.
+// enumerates from beginning until it hits another given item.
+enum Direction2 {
+  Up = 1,
+  Down,
+  Left,
+  Right = "down",
+}
+console.log(Direction2);
+
+// Objects
+// Messy way
+const user: {
+  id: number;
+  name: string;
+} = {
+  id: 1,
+  name: "Alice",
+};
+
+// Cleaner objects
+// Create type first, then just set the variable to that set types
+type Users = {
+  id: number;
+  name: string;
+};
+
+const users: Users = {
+  id: 1,
+  name: "Lacie"
+}
