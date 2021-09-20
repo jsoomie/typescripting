7de525d367df1615e43b69c02e0fe101e22d1eda
Jonathan Soukaseume <sksmejn@gmail.com>
2021-09-20 11:04:21 -0700

    Added Basic primitives to the learning board. Added <any> types, arrays, tuples and tuples array and examples.

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,13 @@
 "use strict";
 let id = 5;
-console.log(id);
+let company = "My Name is Jonathan";
+let isPublished = true;
+let x = "Hello";
+let ids = [1, 2, 3, 4, 5];
+let doggy = [1, "Dog", true];
+let employee;
+employee = [
+    [1, "Jon"],
+    [2, "Mae"],
+    [3, "Joey"]
+];
--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,24 @@
 // Change to ES6 compiled code in tsconfig.json // Default is ES5
 
 // Type is inferred once set
+// Basic primivites
 let id: number = 5;
+let company: string = "My Name is Jonathan";
+let isPublished: boolean = true;
 
-console.log(id);
+// Any Types
+let x: any = "Hello"
+
+// Array TYPE[ARRAY] // Must conform to type
+let ids: number[] = [1, 2, 3, 4, 5];
+
+// Tuples // Types must be in the order stated
+let doggy: [number, string, boolean] = [1, "Dog", true];
+// Tuple Array
+let employee: [number, string] [];
+// Must be in the order stated once again
+employee = [
+  [1, "Jon"],
+  [2, "Mae"],
+  [3, "Joey"]
+]
